8a635829fbbd080600f7b489ed0591bed4fa285d
Pedro <pedro.nbk@gmail.com>
2021-11-22 23:24:35 -0300

    finalizaçao do css

---
+++ script.js
@@ -0,0 +1,18 @@
+let order = [];
+let clickedOrder = [];
+let score = 0;
+
+//0 - verde
+//1 - vermelho
+//2 - amaerlo
+//3 - azul
+
+const blue = document.querySelector('.blue');
+const red = document.querySelector('.red');
+const green = document.querySelector('green');
+const yellow = document.querySelector('.yelllow');
+
+let shuffleOrder = () => {
+    let colorOder = Math.floor(Math.random() * 4);
+    
+}
